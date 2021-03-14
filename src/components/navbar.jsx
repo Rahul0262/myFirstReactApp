04c8d8810eeeb967dacf2505423e759314464209
Rahul0262 <julakanti22@gmail.com>
2021-03-14 13:07:49 +0530

    changed nav bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,6 +5,7 @@ class NavBar extends Component {
 		return (
 			<nav className='navbar navbar-light bg-light'>
 				<span className='navbar-brand mb-0 h1'>Navbar</span>
+				<p>This is branch 1 to be merged to master</p>
 			</nav>
 		);
 	}
